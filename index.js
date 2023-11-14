f80d2454e79962c770d16ec1b3362900aab4f46e
sathish19961412 <sathishfreelanc5@gmail.com>
2023-11-14 16:49:50 +0530

    mern server completed

--- index.js
+++ index.js
@@ -8,11 +8,59 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+//Display Data
+app.get('/',(req,res)=>{
+
+    UserModel.find({})
+    .then(users=>res.json(users))
+    .catch(err=>res.json(err))
+
+})
+
+//Edit Data
+app.get('/getUser/:id',(req,res)=>{
+
+   const id =req.params.id;
+
+   UserModel.findById({_id:id})
+   .then(users=>res.json(users))
+   .catch(err=>res.json(err))
+})
+
+//Update Data
+app.put('/updateUser/:id',(req,res)=>{
+
+    const id =req.params.id;
+
+    UserModel.findByIdAndUpdate({_id:id},{
+        name:req.body.name,
+        email:req.body.email,
+        age:req.body.age})
+    .then(users=>res.json(users))
+    .catch(err=>res.json(err))
+
+})
+
+//Post Data
 app.post('/createUser',(req,res)=>{
+
     UserModel.create(req.body)
     .then(users=>res.json(users))
     .catch(err=>res.json(err))
+
 })
+
+//Delete Data
+app.delete('/deleteUser/:id',(req,res)=>{
+
+    const id =req.params.id;
+    UserModel.findByIdAndDelete({_id:id})
+    .then(users=>res.json(users))
+    .catch(err=>res.json(err))
+
+})
+
 app.listen(3001,()=>{
     console.log('Server is Running')
 })
